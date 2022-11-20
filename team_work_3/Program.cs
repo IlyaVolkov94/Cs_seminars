fbb3ad19b9e31888c151d1b807c04fe9410b1059
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-20 11:30:17 +0300

    team_3

---
+++ team_work_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int negNum=-1*num;
+while (negNum<=num)
+{
+    Console.Write($"{negNum} ");
+    negNum++;
+}
+Console.WriteLine();
