196dbdc59b0e1216ba216493d7468f2859108faa
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-20 10:31:56 +0300

    third

--- exampl001/Program.cs
+++ exampl001/Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
 int square = num*num;
+//Console.WriteLine("Квадрат введенного числа равен"+square);
 Console.WriteLine($"Квадрат введенного числа равен {square}");
+
+if (num>9)
+{
+    Console.WriteLine("Вы ввели двухзначное число");
+}
