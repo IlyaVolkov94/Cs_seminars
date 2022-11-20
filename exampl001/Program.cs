dc88b6e4435eab5a0e6690a4434c36bc4f50249a
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-20 10:36:46 +0300

    four

--- exampl001/Program.cs
+++ exampl001/Program.cs
@@ -4,7 +4,14 @@ int square = num*num;
 //Console.WriteLine("Квадрат введенного числа равен"+square);
 Console.WriteLine($"Квадрат введенного числа равен {square}");
 
-if (num>9)
+if (num>9 && num<100)
 {
     Console.WriteLine("Вы ввели двухзначное число");
 }
+
+while (num<=25)
+{
+    Console.WriteLine($"{num} ");
+    num++;
+}
+Console.WriteLine();
