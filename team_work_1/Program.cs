6ea19cf6945d5103a3ff65725aab219c8f20c9fb
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-20 10:49:22 +0300

    team_1

---
+++ team_work_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целые числа");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+int square = numB*numB;
+if(numA==square)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
