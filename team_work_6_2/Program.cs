48f0f3be6509001ef849b727b3886b2dca227dcc
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-26 11:11:57 +0300

    team_work_2

--- team_work_6_2/Program.cs
+++ team_work_6_2/Program.cs
@@ -2,9 +2,9 @@
 // Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 
 Console.WriteLine("Введите число 1");
-int numA = Convert.ToInt32(Console.Readline());
+int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2");
-int numB = Convert.ToInt32(Console.Readline());
+int numB = Convert.ToInt32(Console.ReadLine());
 int div = numB%numA;
 if(div==0) Console.WriteLine("Второе число кратно первому");
 else Console.WriteLine("Второе число не кратно первому. Остаток:"+div);
