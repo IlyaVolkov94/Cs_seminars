49430aa82e850b7c26d71156709effa8d04de36a
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-26 11:10:25 +0300

    team_work_2

--- team_work_5_2/Program.cs
+++ team_work_5_2/Program.cs
@@ -6,3 +6,10 @@ num=num/100;
 num=num*10;
 num=num + x1;
 Console.WriteLine($"Ответ {num}");
+
+// ИЛИ
+// int num = new Random().Next(100,1000);
+// Console.WriteLine($"Сгенерировано случайное число {num}");
+// int result = num / 100; // 3 цифра (количество сотен)
+// int result1 = num % 10; // 1 цифра (количество единиц)
+// Console.WriteLine($"{result}{result1}");
---
+++ team_work_6_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.WriteLine("Введите число 1");
+int numA = Convert.ToInt32(Console.Readline());
+Console.WriteLine("Введите число 2");
+int numB = Convert.ToInt32(Console.Readline());
+int div = numB%numA;
+if(div==0) Console.WriteLine("Второе число кратно первому");
+else Console.WriteLine("Второе число не кратно первому. Остаток:"+div);
