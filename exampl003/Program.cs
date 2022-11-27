8f360e08e46a245bd35505f97a120434e8c5a0f9
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-27 10:25:53 +0300

    exmpl3

--- exampl003/Program.cs
+++ exampl003/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
 // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
-void CheckKoord (int x, int y)
+void CheckKoord(int x, int y)
 {
-    if (x>0 && y>0) Console.WriteLine("Четверть №1");
-    else if (x>0 && y<0) Console.WriteLine("Четверть №4");
-    else if (x<0 && y>0) Console.WriteLine("Четверть №2");
-    else if (x<0 && y<0) Console.WriteLine("Четверть №3");
+    if (x > 0 && y > 0) Console.WriteLine("Четверть №1");
+    else if (x > 0 && y < 0) Console.WriteLine("Четверть №4");
+    else if (x < 0 && y > 0) Console.WriteLine("Четверть №2");
+    else if (x < 0 && y < 0) Console.WriteLine("Четверть №3");
     else Console.WriteLine("Точка находится на координатной оси");
 }
 
@@ -15,7 +15,7 @@ try     //Если трай не выполняется, программа пе
     int x = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите координату Y");
     int y = Convert.ToInt32(Console.ReadLine());
-    CheckKoord(x,y);
+    CheckKoord(x, y);
 }
 
 catch
