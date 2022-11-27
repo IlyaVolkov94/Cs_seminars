090d86ec4271393590ddde0c08f733a3e89c2c87
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-27 10:36:41 +0300

    exmpl3

--- exampl003/Program.cs
+++ exampl003/Program.cs
@@ -9,6 +9,18 @@ void CheckKoord(int x, int y)
     else Console.WriteLine("Точка находится на координатной оси");
 }
 
+string CheckKoord2(int x, int y)
+{
+    string res;
+    if (x > 0 && y > 0) res = "Четверть №1";
+    else if (x > 0 && y < 0) res = "Четверть №4";
+    else if (x < 0 && y > 0) res = "Четверть №2";
+    else if (x < 0 && y < 0) res = "Четверть №3";
+    else res = "Точка находится на координатной оси";
+    return res;
+}
+
+
 try     //Если трай не выполняется, программа переключается на catch
 {
     Console.WriteLine("Введите координату X");
@@ -16,6 +28,7 @@ try     //Если трай не выполняется, программа пе
     Console.WriteLine("Введите координату Y");
     int y = Convert.ToInt32(Console.ReadLine());
     CheckKoord(x, y);
+    Console.WriteLine(CheckKoord2(x, y));
 }
 
 catch
