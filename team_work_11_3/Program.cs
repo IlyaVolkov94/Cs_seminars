db34b9f22252d632045d5d55735a321d0ab25b78
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-27 19:00:17 +0300

    seminar_3_final

---
+++ team_work_11_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число N и выдает квадраты чисел от 1 до N
+Console.WriteLine("Введите число");
+try
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Квадраты числе от 1 до {num}:");
+    for (int i = 1; i <= num; i++)
+    {
+        int result = i * i;
+        Console.Write($"{result} ");
+    }
+}
+
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
