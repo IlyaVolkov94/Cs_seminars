2d74b45f483349f250fcd03e26ccc6d96526a95b
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-27 14:12:11 +0300

    teamwork_solo

--- team_work_9_3/Program.cs
+++ team_work_9_3/Program.cs
@@ -1,27 +1,34 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
 Console.WriteLine("Введите номер четверти");
-int num = Convert.ToInt32(Console.ReadLine());
-Coordprint(num);
-Console.WriteLine(CoordCheck(num));
 
-
-void Coordprint (int arg)
+try
 {
-    if(arg==1) Console.WriteLine("x>0, y>0");
-    else if (arg==2) Console.WriteLine("x<0, y>0");
-    else if (arg==3) Console.WriteLine("x<0, y<0");
-    else if (arg==4) Console.WriteLine("x>0, y<0");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Coordprint(num);
+    Console.WriteLine(CoordCheck(num));
+}
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
+
+void Coordprint(int arg)
+{
+    if (arg == 1) Console.WriteLine("x>0, y>0");
+    else if (arg == 2) Console.WriteLine("x<0, y>0");
+    else if (arg == 3) Console.WriteLine("x<0, y<0");
+    else if (arg == 4) Console.WriteLine("x>0, y<0");
     else Console.WriteLine("Какого четверти не существует");
 }
 
-string CoordCheck (int arg)
+string CoordCheck(int arg)
 {
     string str;
-    if(arg==1) str= "x>0, y>0";
-    else if (arg==2) str= "x<0, y>0";
-    else if (arg==3) str= "x<0, y<0";
-    else if (arg==4) str= "x>0, y<0";
-    else str= "Какого четверти не существует";
+    if (arg == 1) str = "x>0, y>0";
+    else if (arg == 2) str = "x<0, y>0";
+    else if (arg == 3) str = "x<0, y<0";
+    else if (arg == 4) str = "x>0, y<0";
+    else str = "Какого четверти не существует";
     return str;
 }
