a64e632b1075652d3cf7fffa723a2c9c8f0c3ec2
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-03 12:42:25 +0300

    team_work_14

--- team_work_14_4/Program.cs
+++ team_work_14_4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+Console.WriteLine("Массив:");
+int Size = 8;
+PrintArray(RandomArray(Size));
 
+
+
+int[] RandomArray(int Arg)
+{
+    int[] Array = new int[Arg];
+    for (int i = 0; i < Arg; i++)
+    {
+        Array[i] = new Random().Next(0, 2);
+    }
+    return Array;
+}
+
+void PrintArray(int[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++) Console.Write(Arr[i] + " ");
+
+}
