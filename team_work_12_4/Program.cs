7952e2c8be7eecb8b7aa6ef78dc2d28ea8263482
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-03 12:51:47 +0300

    test

--- team_work_12_4/Program.cs
+++ team_work_12_4/Program.cs
@@ -4,12 +4,22 @@ Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Количество цифр в числе: {countNumber(num)}");
 
+// int countNumber(int arg)
+// {
+//     int count = 0;
+//     while (arg >= 1)
+//     {
+//         arg = arg / 10;
+//         count++;
+//     }
+//     return count;
+// }
+
 int countNumber(int arg)
 {
     int count = 0;
-    while (arg >= 1)
+    for(int i=num;i>=1;i/=10)
     {
-        arg = arg / 10;
         count++;
     }
     return count;
