d2ee05f0eb82146354220f9523f239a9c4691c31
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-03 11:37:40 +0300

    team_work_13

---
+++ team_work_12_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе: {countNumber(num)}");
+
+int countNumber(int arg)
+{
+    int count = 0;
+    while (arg >= 1)
+    {
+        arg = arg / 10;
+        count++;
+    }
+    return count;
+}
---
+++ team_work_13_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {Num} равно {MultNum(Num)}");
+
+int MultNum(int arg)
+{   
+    int Mult = 1;
+    for(int i=1;i<=arg;i++) Mult*=i;
+    return Mult;
+}
