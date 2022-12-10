4b53a04e4b998729f75113f1df86eeb93a9ab5f4
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-10 12:09:35 +0300

    seminar_6

---
+++ team_work_20_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string res = "";
+
+while (num>0)
+{
+    res = Convert.ToString(num%2) + res;
+    num = num / 2;
+}
+Console.WriteLine(res);
