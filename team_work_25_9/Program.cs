f2b2c9159fa47112627c04684fc7e10ab2e25252
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-18 11:53:55 +0300

    seminar_9

---
+++ team_work_25_9/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int SumNumber(int number)
+{
+    int sum = 0;
+    while (true)
+    {
+        sum += number % 10; // sum = sum + number%10;
+        number /= 10; // number = number/10;
+        if (number == 0)
+            break;
+    }
+    return sum;
+}
+
+int SumNumberRec(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + SumNumberRec(number / 10);
+}
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumNumberRec(number);
+Console.WriteLine($"Сумма цифр числа равна: {result}");
---
+++ team_work_26_9/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+Console.WriteLine("Введите основание");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PowRec(numberA, numberB));
+
+
+int Pow(int numberA, int numnerB)
+{
+    int result = 1;
+    while (true)
+    {
+        result *= numberA;
+        numnerB--;
+        if (numnerB == 0)
+            break;
+    }
+    return result;
+}
+// int result = 1;
+// for (int i = 0;i<numberB;i++)
+//     result= result*numberA;
+
+
+
+int PowRec(int numberA, int numberB)
+{
+    if (numberB == 0) return 1;
+    return numberA * PowRec(numberA, numberB - 1);
+}
+
+// numberA * (numbarA * (numberA*(1)))
